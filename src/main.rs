df0772481f61127493082d0844e796d60f54c015
Silvie van Sambeek <silvievansambeek@gmail.com>
2023-06-10 01:32:06 +0200

    changed content of main to try out the variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 4;
+    println!("x is: {} ", x);
+
+    {
+        let x = 2;
+        println!("x is {}", x)
+    }
+    let x = x + 1;
+    println!("x is: {} ", x);
 }
